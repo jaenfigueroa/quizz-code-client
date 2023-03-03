31a923544f77e777d5198787a8a738a7c81b1b9a
jaen <jaenfigueroa03042000@gmail.com>
2023-03-02 19:29:53 -0500

    add styles to challenges and sections

--- src/pages/Challenge/Challenge.jsx
+++ src/pages/Challenge/Challenge.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 // import { useParams } from 'react-router-dom'
 import './Challenge.css'
 import { Header } from './Header/Header';
@@ -13,6 +13,10 @@ export const Challenge = () => {
 
   const [viewResults, setViewResults] = useState(false)
 
+
+  useEffect(()=> {
+    window.scrollTo(0, 0)
+  }, [])
   ////////////////////////////////////
   return (
     <section className='section-exercise'>
@@ -43,10 +47,9 @@ export const Challenge = () => {
           {
             !viewResults ? (
               <>
-                <p className='exercise__text'>Este año los elfos han comprado una máquina que envuelve regalos. Pero… ¡no viene programada! Necesitamos crear un algoritmo que le ayude en la tarea. A la máquina se le pasa un array con los regalos. Cada regalo es un string. Necesitamos que la máquina envuelva cada regalo en papel de regalo y lo coloque en un array de regalos envueltos. El papel de regalo es el símbolo * y para envolver un regalo se coloca el símbolo * de forma que rodee totalmente al string por todos los lados. Por ejemplo:</p>
+                <p className='exercise__text'>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
                 <Code content={`const gifts = ['cat', 'game', 'socks']\nconst wrapped = wrapping(gifts)\n\nconsole.log(wrapped)\n\n/* [\n\t"******\\ncat\\n******",\n\t"*******\\ngame\\n*******",\n\t"********\\nsocks\\n********"\n] */`}/>
-                <p className='exercise__text'>Como ves, el papel de regalo envuelve el string. Por arriba y por abajo, para no dejar ningún hueco, las esquinas también están cubiertas por el papel de regalo. Nota: El carácter \n representa un salto de línea. ¡Ojo! Asegúrate que pones el número correcto de * para envolver completamente el string. Pero no demasiados. Sólo los necesarios para cubrir el string. Ah, y no modifiques (mutes) el array original.</p>
-                <Code content={`const gifts = ['cat', 'game', 'socks']\nconst wrapped = wrapping(gifts)\n\nconsole.log(wrapped)\n\n/* [\n\t"******\\ncat\\n******",\n\t"*******\\ngame\\n*******",\n\t"********\\nsocks\\n********"\n] */`}/>
+                <p className='exercise__text'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
               </>
             ):(
               <p className='exercise__text'>Resultados</p>
