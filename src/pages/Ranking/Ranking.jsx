b56aad48aa4e2dc5bc6e3a31a4e73d9443394f9d
jaen <jaenfigueroa03042000@gmail.com>
2023-03-03 01:05:03 -0500

    add section ranking

--- src/pages/Ranking/Ranking.jsx
+++ src/pages/Ranking/Ranking.jsx
@@ -1,31 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { listUser } from './ListUser'
 import './Ranking.css'
 
 ///////////////////////////////////////////
 export const Ranking = () => {
 
+  const [usersList, setUserlist] = useState(listUser)
 
   ///////////////////////////////////////////
   return (
     <section className='section-ranking'>
 
       {/* TABLA DE INFORMACION EN EL RANKING DEL USUARIO */}
-      <section className='section-ranking__user'>
+      <article className='section-ranking__user'>
         <h4 className='section-ranking__user-title'>Tus resultados</h4>
         <main className='section-ranking__user-main'>
           <div className='section-ranking__user-main-mask'>
-            <img src="" alt="" />
+            <img src="https://avatars.githubusercontent.com/u/69079292?v=4" alt="" />
           </div>
           <div>
             <p>458</p>
             <p>Puntos</p>
           </div>
           <div>
-            <p>1657.°</p>
+            <p>1657°</p>
             <p>Posicion</p>
           </div>
         </main>
-      </section>
+      </article>
 
       {/* TABLA DE TODOS LOS USUARIOS DEL RANKING */}
       <section className='section-ranking__table'>
@@ -37,35 +39,18 @@ export const Ranking = () => {
         </header>
 
         <main>
-          {/* USER 1 */}
-          <article>
-            <span className=''>
-              <img src="" alt="" />
-              <p>Paul Javaloyes</p>
-            </span>
-            <p>15455</p>
-            <p>24</p>
-          </article>
-
-          {/* USER 2 */}
-          <article>
-            <span className=''>
-              <img src="" alt="" />
-              <p>Paul Javaloyes</p>
-            </span>
-            <p>15455</p>
-            <p>24</p>
-          </article>
-
-          {/* USER 3 */}
-          <article>
-            <span className=''>
-              <img src="" alt="" />
-              <p>Paul Javaloyes</p>
-            </span>
-            <p>15455</p>
-            <p>24</p>
-          </article>
+          {
+            usersList && usersList.map((user, index) => (
+              <article key={index}>
+                <span>
+                  <img src={`https://randomuser.me/api/portraits/men/${ Math.floor(Math.random() * 99) + 1}.jpg`} alt="" />
+                  <p>{user.name}</p>
+                </span>
+                <p>{user.points}</p>
+                <p>{user.challenges}</p>
+              </article>
+            ))
+          }
         </main>
       </section>
 
