32861bd45b1e832d58ed57b29713e21cb94dd502
Luis Angel Garcia Louis <ludevgarcialouis@gmail.com>
2023-03-05 00:51:40 -0400

    Creating Quizz Test

--- src/pages/Quizz/QUizz.jsx
+++ src/pages/Quizz/QUizz.jsx
@@ -1,57 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Quizz.css";
 import questions from "./data/questions";
-import { useParams } from "react-router-dom";
 
 export const Quizz = () => {
-	const [score, setScore] = useState(0);
-	const [time, setTime] = useState({ m: 5, s: 0 });
-	const { number } = useParams();
+	
+	// const [time, setTime] = useState(10);
+	// useEffect(() => {
+	// }, [time]);
 
-	const checkAnswer = (isCorrect) => {
-		if (isCorrect) {
-			setScore(score + 1);
-		}
-	};
+	let time = 10;
+	const timeInterval = setInterval(() => {
+		if (time === 0) return clearInterval(timeInterval);
+		console.log(time -= 1);
+	}, 1500);
 
 	return (
-		<main className="main-quizz">
-			<section className="section-quizz">
-				<div className="status-quizz">
-					{/* <h1 className="question-title">
-						Question {number} of {questions.length}
-					</h1> */}
-					<span className="question-timer">
-						{time.m} : {time.s}
-					</span>
-				</div>
-
-				<article className="">
-					<h2 className="ask-title">{questions[number].title}</h2>
-
-					{questions[number].img && (
-						<img
-							className="picture-question"
-							src={questions[number].img}
-							alt={questions[number].title}
-						/>
-					)}
-
-					<div className="optins">
-						{questions[number].options.map(
-							({ answertext, isCorrect }, index) => (
-								<button
-									className="btn-option"
-									key={index}
-									onClick={() => checkAnswer(isCorrect)}
-								>
-									{answertext}
-								</button>
-							),
-						)}
-					</div>
-				</article>
-			</section>
-		</main>
+		<section className="section-quizz">
+			<div className="quizz-timer">{time}s</div>
+			<h2 className="ask-title">{questions[1].title}</h2>
+			<div className="ask-options">
+				{questions[0].options.map((option, index) => (
+					<button
+						key={`${index}-${option.answertext}`}
+						className={`ask-option-btn-${index}`}
+					>
+						{option.answertext}
+					</button>
+				))}
+			</div>
+		</section>
 	);
 };
--- src/pages/Quizz/data/questions.js
+++ src/pages/Quizz/data/questions.js
@@ -5,43 +5,40 @@ const questions = [
       options: [
         { answertext: "Navegador del servidor", isCorrect: false },
         { answertext: "Navegador del Cliente", isCorrect: true },
+        { answertext: "Navegador del Chino", isCorrect: true },
       ],
     },
+
     {
-      title: "¿Qué empresa desarrolló JavaScript",
-      img: "https://stackify.com/wp-content/uploads/2018/10/JavaScript-Tutorials-for-Beginners.jpg",
-  
+      title: "¿En qué lugar se ejecuta generalmente el código JavaScript?",
+      img: "https://kinsta.com/es/wp-content/uploads/sites/8/2022/10/javascript-http-request.png",
       options: [
-        { answertext: "Platón", isCorrect: false },
-        { answertext: "Sócrates", isCorrect: false },
+        { answertext: "Navegador del servidor", isCorrect: false },
+        { answertext: "Navegador del Cliente", isCorrect: true },
+        { answertext: "Navegador del Chino", isCorrect: true },
       ],
     },
+
     {
-      title: `¿Cuál es el país más grande y el más pequeño del mundo?`,
-      img: "https://phrase.com/wp-content/uploads/2018/01/react-localization-i18n.jpg",
+      title: "¿En qué lugar se ejecuta generalmente el código JavaScript?",
+      img: "https://kinsta.com/es/wp-content/uploads/sites/8/2022/10/javascript-http-request.png",
       options: [
-        { answertext: "Rusia y Vaticano", isCorrect: true },
-        { answertext: "Platón", isCorrect: false },
+        { answertext: "Navegador del servidor", isCorrect: false },
+        { answertext: "Navegador del Cliente", isCorrect: true },
+        { answertext: "Navegador del Chino", isCorrect: true },
       ],
     },
-  
+
     {
-      title: `¿Cuál es el libro más vendido en el mundo después de la Biblia?`,
-      img: "https://phrase.com/wp-content/uploads/2018/01/react-localization-i18n.jpg",
+      title: "¿En qué lugar se ejecuta generalmente el código JavaScript?",
+      img: "https://kinsta.com/es/wp-content/uploads/sites/8/2022/10/javascript-http-request.png",
       options: [
-        { answertext: "El Señor de los Anillos", isCorrect: false },
-        { answertext: "Don Quijote de la Mancha", isCorrect: true },
+        { answertext: "Navegador del servidor", isCorrect: false },
+        { answertext: "Navegador del Cliente", isCorrect: true },
+        { answertext: "Navegador del Chino", isCorrect: true },
       ],
     },
-  
-    {
-      title: `La sal común está formada por dos elementos, ¿cuáles son?`,
-      img: "https://raygun.com/blog/wp-content/uploads/2016/07/Callums-post-on-Typescript.png",
-      options: [
-        { answertext: "Sodio y potasio", isCorrect: false },
-        { answertext: "Sodio y carbono", isCorrect: false },
-      ],
-    }
+    
   ];
   
   export default questions;
