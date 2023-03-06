9052fe1764aa5b8ddd708559881c483d6756be84
jaen <jaenfigueroa03042000@gmail.com>
2023-03-05 19:21:22 -0500

    change themes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import { AppContext } from '../../context/AppContext'
 import './Header.css'
 import { User } from './User/User'
@@ -19,16 +19,16 @@ export const Header = () => {
           {
             isAuthenticated ? (
               <>
-                <Link className='header__item' to='/home'>Home</Link>
+                <NavLink className={({isActive})=>isActive? 'header__item--active':'header__item'} to='/home'>Home</NavLink>
               </>
             ) :(
               <>
-                <Link className='header__item' to='/login'>Iniciar Sesion</Link>
-                <Link className='header__item' to='/register'>Registrarse</Link>
+                <NavLink className={({isActive})=>isActive? 'header__item--active':'header__item'} to='/login'>Iniciar Sesion</NavLink>
+                <NavLink className={({isActive})=>isActive? 'header__item--active':'header__item'} to='/register'>Registrarse</NavLink>
               </>
             )
           }
-          <Link className='header__item' to='/ranking'>Ranking</Link>
+          <NavLink className={({isActive})=>isActive? 'header__item--active':'header__item'} to='/ranking'>Ranking</NavLink>
         </ul>
         {
           isAuthenticated && (
