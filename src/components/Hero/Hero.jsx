2f1638ee1fb6130f0e8d4b033f9252b4b7c9c24a
jaen <jaenfigueroa03042000@gmail.com>
2023-03-07 18:15:41 -0500

    reorganize the sections

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react'
+import { AppContext } from '../../context/AppContext'
+import './Hero.css'
+
+//////////////////////////////////
+export const Hero = () => {
+
+  const {isAuthenticated} = useContext(AppContext)
+
+  //////////////////////////////////
+  return (
+    <section className='section-hero'>
+
+      <div className='section-hero__container'>
+
+        <p className='section-hero__text'>¡Acepta los retos y lleva tus habilidades de <span>HTML, CSS y JavaScript</span> al siguiente nivel!</p>
+        
+        {
+          isAuthenticated ? (
+            <a href='#section-categories' className='section-hero__button-acept'>Aceptar</a>
+          ) : (
+            <Link to='/login' className='section-hero__button-acept'>Aceptar</Link>
+          )
+        }
+
+      </div>
+    </section>
+  )
+}
