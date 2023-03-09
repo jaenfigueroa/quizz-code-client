9f7057e9c4a563b4df8a540d6ff03442cdbd7ae1
jaen <jaenfigueroa03042000@gmail.com>
2023-03-09 16:08:12 -0500

    change urlServer

--- src/helpers/global.js
+++ src/helpers/global.js
@@ -1,3 +1,4 @@
 export const global = {
-  urlServer: 'http://localhost:5000/api'
+  // urlServer: 'http://localhost:5000/api',
+  urlServer: 'https://quizcode-backend-production.up.railway.app/api'
 }
