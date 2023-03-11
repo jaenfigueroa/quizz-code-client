6318e7f7281095ca6c0e4a142fb9884f5d1c68c4
jaen <jaenfigueroa03042000@gmail.com>
2023-03-11 10:15:19 -0500

    chage footer text

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './Footer.css'
 
+////////////////////////////////////////////////////
 export const Footer = () => {
+
+  const year = new Date().getFullYear()
+
+
+
+  ////////////////////////////////////////////////////
   return (
     <footer className='footer'>
       <p className='footer__text'>
-        Made with <span className='footer__heart'>❤</span> by Developers Team{' '}
+        &copy; {year}. Todos los derechos reservados.
       </p>
     </footer>
   )
