56b182e682427c9dbb03fe6001a175b3cd2a23db
Rafael Gómez <satrgomez7@gmail.com>
2023-03-20 19:04:42 -0400

    feat: implement Admin add question
    Co-Authored-By: Jaen Figueroa <69079292+jaenfigueroa@users.noreply.github.com>

---
+++ src/components/Admin/NewQuestionForm/NewQuestionFormInputField.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const NewQuestionFormInputField = ({ title, name, type, placeholder }) => {
+  const min = type === "number" ? 1 : null;
+  const max = type === "number" ? 5 : null;
+
+  return (
+    <div className="section-admin__block">
+      <span className="section-admin__title">{title}</span>
+      {type === "number" ? (
+        <input
+          className="section-admin__textarea"
+          name={name}
+          type={type}
+          min={min}
+          max={max}
+          placeholder={placeholder}
+        />
+      ) : (
+        <textarea
+          className="section-admin__textarea"
+          name={name}
+          type={type}
+          placeholder={placeholder}
+          min={min}
+          max={max}
+        />
+      )}
+    </div>
+  );
+};
+
+export default NewQuestionFormInputField;
---
+++ src/helpers/sendNewQuestionForm.js
@@ -0,0 +1,60 @@
+import { global } from "./global";
+
+export const sendNewQuestionForm = async (body, userId) => {
+  const options = [
+    {
+      contentType: body["option-1-content-type"],
+      contentLanguage: body["option-1-content-language"],
+      answer: body["option-1"],
+    },
+    {
+      contentType: body["option-2-content-type"],
+      contentLanguage: body["option-2-content-language"],
+      answer: body["option-2"],
+    },
+    {
+      contentType: body["option-3-content-type"],
+      contentLanguage: body["option-3-content-language"],
+      answer: body["option-3"],
+    },
+    {
+      contentType: body["option-4-content-type"],
+      contentLanguage: body["option-4-content-language"],
+      answer: body["option-4"],
+    },
+    {
+      contentType: body["option-5-content-type"],
+      contentLanguage: body["option-5-content-language"],
+      answer: body["option-5"],
+    },
+  ];
+  const requestBody = {
+    category: body.category,
+    question: body.question,
+    optionalCode: body["optional-code"],
+    options,
+    correctAnswer: body["correct-answer"],
+  };
+  try {
+    const response = await fetch(
+      `${global.urlServer}/questions?user-id=${userId}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody),
+      }
+    );
+    const data = await response.json();
+    return {
+      status: response.status,
+      data,
+    };
+  } catch (error) {
+    return {
+      status: "error",
+      message: "No se pudo enviar la solicitud",
+    };
+  }
+};
