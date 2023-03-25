32493583e981132649b6cffd13dc2c0cc8fa8159
jaen <jaenfigueroa03042000@gmail.com>
2023-03-25 07:55:53 -0500

    feat: add dotenv

--- src/helpers/global.js
+++ src/helpers/global.js
@@ -1,4 +1,3 @@
 export const global = {
-  // urlServer: 'http://localhost:5000/api'
-  urlServer: 'https://quizcode-backend-production.up.railway.app/api'
+  urlServer: process.env.REACT_APP_API_URL
 }
